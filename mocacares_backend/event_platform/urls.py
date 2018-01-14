cdeac95b1456d046946794bc3412faf9a3426fa7
royl8 <royl8@qq.com>
2018-01-13 22:03:10 -0800

    - add some urls - start implementing views

--- mocacares_backend/event_platform/urls.py
+++ mocacares_backend/event_platform/urls.py
@@ -1,6 +1,10 @@
-from django.conf.urls import url, include
+from django.conf.urls import include, url
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    
+    url(r'^api/event/eventDetail', views.get_event),
+
+    url(r'^api/event/commentAdd', views.post_comment),
 ]
--- mocacares_backend/event_platform/views/__init__.py
+++ mocacares_backend/event_platform/views/__init__.py
@@ -1,5 +1,8 @@
 from .views_event import *
 from .views_user import *
+from ..models import *
+from django.core.exceptions import ObjectDoesNotExist
+from django.http import HttpResponse, JsonResponse
 
 
 def get_user(request):
@@ -10,8 +13,18 @@ def get_comment_list(request):
     pass
 
 
-def post_comment(request):
-    pass
+def get_events_by_type(request):
+    event_type = request.GET['type']
+    return HttpResponse(status=200)
 
 
 def get_event(request):
+    event_id = request.GET['aid']
+    try:
+        event = Event.objects.get(pk=event_id)
+    except ObjectDoesNotExist:
+        return HttpResponse(status=404)
+
+
+def post_comment(request):
+    pass

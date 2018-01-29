5198694430e5ac6e5d8a8b24bd68eedbc41ccdf2
royl8 <royl8@qq.com>
2018-01-28 17:09:29 -0800

    - move redis subscription logic out rather than inside connection handling logic - use a map to store uid->socket mapping - refactoring

--- socket/socket.js
+++ socket/socket.js
@@ -9,52 +9,55 @@ var HOST = '0.0.0.0';
 var PORT = 4000;
 
 
-var connections = new Map();
+var connectionMapByUid = new Map();
 
-
-// Create a server instance, and chain the listen function to it
-// The function passed to net.createServer() becomes the event handler for the 'connection' event
-// The sock object the callback function receives UNIQUE for each connection
-net.createServer(function(sock) {
+function connectionHandler(sock) {
     // We have a connection - a socket object is assigned to the connection automatically
     var clientId = sock.remoteAddress + ':' + sock.remotePort;
     console.log('CONNECTED: ' + clientId);
-    connections.set(clientId, sock);
-    
-    sub.on('message', function (channel, message) {
-        message_json = JSON.parse(message.replace(/\'/g, '"'));
-        sock.write(JSON.stringify({
-            code: 2,
-            msg: '聊天信息',
-            info: {
-                "id": String(message_json.id),
-                "fid": String(message_json.fid),
-                "sid": "1",
-                "msg": message_json.msg,
-                "c_time": message_json.c_time,
-                "status": message_json.status,
-                "f_username": message_json.f_username,
-                "f_img": message_json.f_img,
-                "s_username": message_json.s_username,
-                "s_img": message_json.s_img
-            }
-        }));
-    });
 
     // Add a 'data' event handler to this instance of socket
     sock.on('data', function(data) {
-        console.log('DATA ' + sock.remoteAddress + ': ' + data);
-        // Write the data back to the socket, the client will receive it as data from the server
-        sock.write('You said "' + data + '"');
+        console.log('DATA from ' + sock.remoteAddress + ': \n' + data + '\n');
+        var json = JSON.parse(data);
+        if (json.hasOwnProperty('uid')) {
+            connectionMapByUid.set(parseInt(json.uid), sock);
+            console.log(connectionMapByUid.keys());
+        }
     });
     
     // Add a 'close' event handler to this instance of socket
     sock.on('close', function(data) {
         console.log('CLOSED: ' + clientId);
-        connections.delete(clientId);
     });
-    
-}).listen(PORT, HOST);
+}
+
+// Create a server instance, and chain the listen function to it
+// The function passed to net.createServer() becomes the event handler for the 'connection' event
+// The sock object the callback function receives UNIQUE for each connection
+var server = net.createServer(connectionHandler);
+
+server.listen(PORT, HOST);
+
+sub.on('message', function (channel, message) {
+    message_json = JSON.parse(message.replace(/\'/g, '"'));
+    connectionMapByUid.get(message_json.sid).write(JSON.stringify({
+        code: 2,
+        msg: '聊天信息',
+        info: {
+            "id": String(message_json.id),
+            "fid": String(message_json.fid),
+            "sid": String(message_json.sid),
+            "msg": message_json.msg,
+            "c_time": message_json.c_time,
+            "status": message_json.status,
+            "f_username": message_json.f_username,
+            "f_img": message_json.f_img,
+            "s_username": message_json.s_username,
+            "s_img": message_json.s_img
+        }
+    }));
+});
 
 console.log('Server listening on ' + HOST +':'+ PORT);
 

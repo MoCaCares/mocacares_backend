ede4b2467c11f1210096d6752e349792bb0da3c8
royl8 <royl8@qq.com>
2018-01-29 15:27:17 -0800

    fix typo

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 # assume in python virtual environment
 
-python variora/manage.py makemigrations
-python variora/manage.py migrate
-sudo env/bin/python variora/manage.py collectstatic --noinput
+python mocacares_backend/manage.py makemigrations
+python mocacares_backend/manage.py migrate
+sudo env/bin/python mocacares_backend/manage.py collectstatic --noinput
 
 sudo service memcached restart
 sudo service redis-server restart

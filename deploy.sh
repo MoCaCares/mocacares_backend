5342a1415c0affccd7dc81373310b7e76e4b1d15
royl8 <royl8@qq.com>
2018-02-02 12:18:12 -0800

    update deploy script to restart the nodejs socket server

--- deploy.sh
+++ deploy.sh
@@ -7,3 +7,7 @@ sudo env/bin/python mocacares_backend/manage.py collectstatic --noinput
 sudo service memcached restart
 sudo service redis-server restart
 sudo service apache2 restart
+
+pm2 stop all
+pm2 delete all
+pm2 start socket/socket.js

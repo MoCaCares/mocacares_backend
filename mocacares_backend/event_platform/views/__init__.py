0d8e84d0babb3eff3db569183fa268fec18cd67a
royl8 <royl8@qq.com>
2018-02-12 15:33:50 -0800

    refactoring

--- mocacares_backend/event_platform/views/__init__.py
+++ mocacares_backend/event_platform/views/__init__.py
@@ -24,18 +24,12 @@ def post_comment(request):
 
     user = get_user(request)
     if isinstance(user, AnonymousUser):
-        return JsonResponse({
-            'code': 0,
-            'msg': 'You need to log in to post comment'
-        }, status=400)
+        return response_of_failure(msg='you need to log in to post comment')
 
     event = Event.objects.get(pk=request.POST['aid'])
     comment = Comment(poster=user, target_event=event, content=request.POST['content'])
     comment.save()
-    return JsonResponse({
-        'code': 1,
-        'msg': 'success'
-    })
+    return response_of_success(msg='success')
 
 
 def post_feedback(request):
@@ -46,18 +40,12 @@ def post_feedback(request):
         receiver_list=['royl8@qq.com']
     ).start()
 
-    return JsonResponse({
-        'code': 1,
-        'msg': 'feedback sent successfully'
-    }, status=200)
+    return response_of_success(msg='feedback sent successfully')
 
 
 def book_event(request):
     if 'aid' not in request.POST or '_token' not in request.POST or 'type' not in request.POST:
-        return JsonResponse({
-            'code': 0,
-            'msg': 'invalid request'
-        }, status=400)
+        return response_of_failure(msg='missing fields(s)')
     user = get_user(request)
     event = Event.objects.get(pk=request.POST['aid'])
     if request.POST['type'] == '1':
@@ -69,10 +57,7 @@ def book_event(request):
             'code': 0,
             'msg': 'invalid request'
         }, status=400)
-    return JsonResponse({
-        'code': 1,
-        'msg': 'success'
-    })
+    return response_of_success(msg='success') 
 
 
 def get_booked_events(request):
